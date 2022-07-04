b21874870eac17b08ae5e5d12f62c49e82aaa6e9
PKamotho <107169106+PKamotho@users.noreply.github.com>
2022-07-04 10:45:47 +0300

    Add files via upload

---
+++ practical.php
@@ -0,0 +1,20 @@
+<?php
+$txt="This is a practical";
+$name="kamotho";
+$school="ackedy";
+$x=5;
+
+function teachersNames(){
+    $name="Atwoli";
+    echo"i am known as $name <br>";
+}
+teachersNames();
+
+function carNames(){
+    $cars="tesla";
+    echo"i drive a $cars <br>";
+}
+carNames();
+
+echo "$txt done by  $name";
+?>
